3c5b9c7e09175d571a7fd38de2952cf0b2cfbc18
Carlos Adrian <Teshynil@users.noreply.github.com>
2022-09-13 14:23:33 -0500

    Initial commit

---
+++ assets/config.js
@@ -0,0 +1,27 @@
+let config = {
+  "Title": "Central Dungeon - Links",
+  "Name": "Central Dungeon",
+  "Description": "Nuestros Links",
+  "links": [
+    /*{
+      "Title": "Website",
+      "URL": "https://centraldungeon.org",
+      "icon_classes": "fas fa-link",
+    },*/
+    {
+      "Title": "Discord",
+      "URL": "https://discord.gg/DeDEAytjv9",
+      "icon_classes": "fab fa-discord",
+    },
+    {
+      "Title": "Facebook",
+      "URL": "https://www.facebook.com/CentralDungeon",
+      "icon_classes": "fab fa-facebook"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/channel/UCE9uWOAHVmQV5SN4gAYXFzQ",
+      "icon_classes": "fab fa-youtube"
+    },
+  ]
+}
